97ee870b6089a2e5b323b64ed5fb171807ab91dc
lia <liamillul@gmail.com>
2023-09-18 12:06:16 +0300

    moduls commit

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('hi')
+
+
+# create_screen()
+# screen_stay()
+
+from screen import *
+import pygame
+
+gridDisplay = pygame.display.set_mode((1000, 500))
+pygame.display.get_surface().fill((138, 201, 38))  # background
+
+matrix = [[1, 1, 5, 1],
+          [1, 0 ,0 ,1],
+          [1 ,1 ,0 ,1],
+          [1 ,1 ,1 ,1]]
+
+grid_node_width = 20
+grid_node_height = 20
+
+
+def create_square(x, y, color):
+    pygame.draw.rect(gridDisplay, color, [x, y, grid_node_width, grid_node_height])
+
+
+pygame.display.update()
+screen_stay()
--- screen.py
+++ screen.py
@@ -1,1 +1,19 @@
-screen
+import pygame
+from consts import *
+
+
+def create_screen():
+    pygame.init()
+    surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_LENGTH))
+    surface.fill(GREEN)
+    pygame.display.flip()
+
+
+def screen_stay():
+    running = True
+    while running:
+        # for loop through the event queue
+        for event in pygame.event.get():
+            # Check for QUIT event
+            if event.type == pygame.QUIT:
+                running = False
